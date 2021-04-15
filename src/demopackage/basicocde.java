b889bb6d866cbf1b0be4612fe90afcbcd4a1b366
Candy007Surya <saisurya17444@gmail.com>
2021-04-15 21:19:02 +0530

    orginal file this is

---
+++ src/demopackage/basicocde.java
@@ -0,0 +1,21 @@
+package demopackage;
+
+
+
+public class basicocde {
+
+	public static void main(String[] args)
+   {
+		addtion(2, 4, 8);
+		
+	}
+	
+	
+	public static int addtion(int x , int y, int c)
+	{
+		int k = x+y+c ;
+		 System.out.println("addtion of this numbers is :---" + k);
+		return k ;
+	}
+
+}
